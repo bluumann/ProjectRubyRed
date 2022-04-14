4dd8d338f696b57b9ea60c6516f60760d358f6d8
Peter <p.kosa089@mybvc.ca>
2022-04-14 17:40:55 -0600

    Add basic structure for owner

--- serverStart.js
+++ serverStart.js
@@ -2,18 +2,44 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + "/index.html")
-})
+/*** LANDING PAGE ***/
+// Route to landing page
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-var server = app.listen(8080, function(){
-    console.log("Server is running!")
-    var host = server.address().address;
-    var port = server.address().port;
-})
+// Route to logged in page
+app.post('/logged', function (req, res) {
+  res.sendFile(__dirname + '/logged.html');
+});
 
+/*** USER PAGE ***/
+// Route to logged in an user page
+app.get('/user', function (req, res) {
+  res.sendFile(__dirname + '/user.html');
+});
 
+/*** OWNER PAGE ***/
+// Route to owner's home page (when logged in as an owner)
+app.get('/owner/home', function (req, res) {
+  res.sendFile(__dirname + '/owner/home.html');
+});
 
+// Route to owner's profile page
+app.get('/owner/profile', function (req, res) {
+  res.sendFile(__dirname + '/owner/profile.html');
+});
+
+// Route to owner's properties page
+app.get('/owner/properties', function (req, res) {
+  res.sendFile(__dirname + '/owner/properties.html');
+});
+
+var server = app.listen(8080, function () {
+  console.log('Server is running on port 8080');
+  var host = server.address().address;
+  var port = server.address().port;
+});
