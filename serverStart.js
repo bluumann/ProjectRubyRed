ab9cc10463b9b5b78e294fb93e922b5c528b56c2
Peter <p.kosa089@mybvc.ca>
2022-04-22 16:39:40 -0600

    Create workspace funcionality fully integrated into current user and selected property

--- serverStart.js
+++ serverStart.js
@@ -63,43 +63,48 @@ app.get('/', function (req, res) {
 /*** LOGIN ***/
 //Obtain login info and check credentials
 app.post('/login', function (req, res) {
-    //Get login details
-    var accountInfo = {
-        email: req.body.email,
-        password: req.body.password
-    };
+  //Get login details
+  var accountInfo = {
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-    //Create a variable to hold who is currently logged in
-    global.currentUser;
+  //Create a variable to hold who is currently logged in
+  global.currentUser;
 
-    var flag = false; //Create a flag for if/when we find the user
+  var flag = false; //Create a flag for if/when we find the user
 
-    //Go through users to find matching username and password
-    for (let i = 0; i < obj.Users.length; i++) {
-        if (obj.Users[i].email == accountInfo.email && obj.Users[i].password == accountInfo.password) {
-            flag = true;
-            req.session.loggedin = true;
-            req.session.name = obj.Users[i].fName;
-            currentUser = obj.Users[i];
-            //console.log(currentUser);
-            break;
-        }
+  //Go through users to find matching username and password
+  for (let i = 0; i < obj.Users.length; i++) {
+    if (
+      obj.Users[i].email == accountInfo.email &&
+      obj.Users[i].password == accountInfo.password
+    ) {
+      flag = true;
+      req.session.loggedin = true;
+      req.session.name = obj.Users[i].fName;
+      currentUser = obj.Users[i];
+      //console.log(currentUser);
+      break;
     }
+  }
 
-    //Go through owners to find matching username and password if not found in users
-    if (!flag) {
-        for (let i = 0; i < obj.Owners.length; i++) {
-            if (obj.Owners[i].email == accountInfo.email && obj.Owners[i].password == accountInfo.password) {
-                flag = true;
-                req.session.loggedin = true;
-                req.session.name = obj.Owners[i].fName;
-                currentUser = obj.Owners[i];
-                //console.log(currentUser);
-                break;
-            }
-        }
+  //Go through owners to find matching username and password if not found in users
+  if (!flag) {
+    for (let i = 0; i < obj.Owners.length; i++) {
+      if (
+        obj.Owners[i].email == accountInfo.email &&
+        obj.Owners[i].password == accountInfo.password
+      ) {
+        flag = true;
+        req.session.loggedin = true;
+        req.session.name = obj.Owners[i].fName;
+        currentUser = obj.Owners[i];
+        //console.log(currentUser);
+        break;
+      }
     }
-
+  }
 
   //Go through owners to find matching username and password if not found in users
   if (!flag) {
@@ -165,8 +170,7 @@ app.post('/SignUp', urlencodedParser, function (req, res) {
       properties: [],
     };
     obj.Owners.push(accountInfo);
-
-  }  
+  }
   for (let i = 0; i < obj.Owners.length; i++) {
     if (
       obj.Owners[i].email == accountInfo.email &&
@@ -228,11 +232,17 @@ app.get('/owner/properties', function (req, res) {
 });
 
 //CURRENT USER API
-app.get('/currentUser', urlencodedParser, function(req, res){
-  res.send(global.currentUser)
-  console.log(global.currentUser)
-  console.log("API called! Delivering info about " + global.currentUser.fName)
-})
+app.get('/currentUser', urlencodedParser, function (req, res) {
+  res.send(global.currentUser);
+  console.log(global.currentUser);
+  console.log('API called! Delivering info about ' + global.currentUser.fName);
+});
+
+app.get('/currentproperty', urlencodedParser, function (req, res) {
+  res.send(global.currentUser.properties);
+  console.log(global.currentUser);
+  //console.log('API called! Delivering info about ' + global.currentUser.fName);
+});
 
 /*** PROPERTY PAGES ***/
 
@@ -243,31 +253,34 @@ app.get('/property/create', function (req, res) {
 
 // ROUTE TO "PROPERTY CREATED" PAGE
 app.post('/property/property-created', urlencodedParser, function (req, res) {
-
   var property = {
-  name: req.body.name,
-  address: req.body.address,
-  type: req.body.type,
-  park: req.body.park,
-  transport: req.body.transport,
-  smoking: req.body.smoking,
-  seats: req.body.individuals,
-  availabilityFrom: req.body.availabilityFrom,
-  availabilityTo: req.body.availabilityTo,
-  leaseterm: req.body.leaseterm,
-  price: req.body.price,
-  workspaces: []
-};
-//console.log("Test"); //debug
-currentUser.properties.push(property);
-console.log(currentUser); //debug
+    name: req.body.name,
+    address: req.body.address,
+    type: req.body.type,
+    park: req.body.park,
+    transport: req.body.transport,
+    smoking: req.body.smoking,
+    seats: req.body.individuals,
+    availabilityFrom: req.body.availabilityFrom,
+    availabilityTo: req.body.availabilityTo,
+    leaseterm: req.body.leaseterm,
+    price: req.body.price,
+    workspaces: [],
+  };
+  //console.log("Test"); //debug
+  currentUser.properties.push(property);
+  console.log(currentUser); //debug
 
-//Update the file with the new information
-fs.writeFile(path.join(__dirname, 'data', 'data.json'), JSON.stringify(obj, null, 2), propertyAdded);
-function propertyAdded() {
-console.log("New property added.");
-//res.redirect('/propertyIn'); //placeholder
-}
+  //Update the file with the new information
+  fs.writeFile(
+    path.join(__dirname, 'data', 'data.json'),
+    JSON.stringify(obj, null, 2),
+    propertyAdded
+  );
+  function propertyAdded() {
+    console.log('New property added.');
+    //res.redirect('/propertyIn'); //placeholder
+  }
 });
 
 // ROUTE TO "UPDATE PROPERTY" PAGE
@@ -275,125 +288,127 @@ app.get('/property/update', function (req, res) {
   res.sendFile(__dirname + '/property/update-property.html');
 });
 
-
-
 /*** WORKSPACE PAGES ***/
 
 // ROUTE TO "CREATE WORKSPACE" PAGE
 app.get('/workspace/create', function (req, res) {
-    res.sendFile(__dirname + '/workspace/create-workspace.html');
+  res.sendFile(__dirname + '/workspace/create-workspace.html');
 });
 
 // ROUTE TO "WORKSPACE CREATED" PAGE
 app.post('/workspace/workspace-created', urlencodedParser, function (req, res) {
-    var workspace = {
-        id: shortid.generate(), // Auto-generate ID
-        name: req.body.name,
-        type: req.body.type,
-        smoking: req.body.smoking,
-        seats: req.body.seats,
-        availability: req.body.availability,
-        leaseterm: req.body.leaseterm,
-        price: req.body.price,
-        listed: req.body.listed,
-    };
+  var propertyName = req.body.property;
+  var workspace = {
+    id: shortid.generate(), // Auto-generate ID
+    name: req.body.name,
+    type: req.body.type,
+    smoking: req.body.smoking,
+    seats: req.body.seats,
+    availability: req.body.availability,
+    leaseterm: req.body.leaseterm,
+    price: req.body.price,
+    listed: req.body.listed,
+  };
 
-    ownerIndex = obj.Owners.indexOf(currentUser, 0); // Index of currentUser within Owners[]
+  const currentProperty = currentUser.properties.find( // Find property that matches propertyName
+    property => property.name === propertyName 
+  );
 
+  propertyIndex = currentUser.properties.indexOf(currentProperty, 0); // Index of currentProperty within currentUser's properties
 
-    obj.Owners[ownerIndex].properties[0].workspaces.push(workspace);
+  ownerIndex = obj.Owners.indexOf(currentUser, 0); // Index of currentUser within Owners[]
 
+  obj.Owners[ownerIndex].properties[propertyIndex].workspaces.push(workspace);
 
-    //Update data.json with the new information
-    fs.writeFile(
-        path.join(__dirname, 'data', 'data.json'),
-        JSON.stringify(obj, null, 2),
-        workspaceCreated
-      );
-      function workspaceCreated() {
-        console.log('New workspace created.');
-        res.send('A new workspace has been created.');
-      }
-    });
-    
+  //Update data.json with the new information
+  fs.writeFile(
+    path.join(__dirname, 'data', 'data.json'),
+    JSON.stringify(obj, null, 2),
+    workspaceCreated
+  );
+  function workspaceCreated() {
+    console.log('New workspace created.');
+    res.send('A new workspace has been created.');
+  }
+});
+
 // ROUTE TO "UPDATE WORKSPACE" PAGE
 app.get('/workspace/update', function (req, res) {
-    res.sendFile(__dirname + '/workspace/update-workspace.html');
+  res.sendFile(__dirname + '/workspace/update-workspace.html');
 
-// Return data.json to be used in HTML
-app.get('/data/data.json', function (req, res) {
+  // Return data.json to be used in HTML
+  app.get('/data/data.json', function (req, res) {
     res.sendFile(__dirname + '/data/data.json');
   });
-  
-// ROUTE TO "WORKSPACE UPDATED" PAGE
-app.post('/workspace/workspace-updated', urlencodedParser, UpdateWorkspace);
-  
-    // Get data from "Update Workspace" form and add it to "obj"
-    function UpdateWorkspace(req, res) {
-        var id = req.body.id;
-        var name = req.body.name;
-        var type = req.body.type;
-        var smoking = req.body.smoking;
-        var seats = req.body.seats;
-        var availability = req.body.availability;
-        var leaseterm = req.body.leaseterm;
-        var price = req.body.price;
-        var listed = req.body.listed;
-  
-        const workspaceToUpdate = obj.Workspaces.find(
-            workspace => workspace.id === id
-        );
-  
-        workspaceToUpdate.name = name;
-        workspaceToUpdate.type = type;
-        workspaceToUpdate.smoking = smoking;
-        workspaceToUpdate.seats = seats;
-        workspaceToUpdate.availability = availability;
-        workspaceToUpdate.leaseterm = leaseterm;
-        workspaceToUpdate.price = price;
-        workspaceToUpdate.listed = listed;
 
-  
-        let data = JSON.stringify(obj, null, 1);
-        fs.writeFile('data/data.json', data, Updated);
-        function Updated() {
-        console.log('Workspace updated.');
-        res.send('Workspace has been updated.');
-        }
+  // ROUTE TO "WORKSPACE UPDATED" PAGE
+  app.post('/workspace/workspace-updated', urlencodedParser, UpdateWorkspace);
+
+  // Get data from "Update Workspace" form and add it to "obj"
+  function UpdateWorkspace(req, res) {
+    var id = req.body.id;
+    var name = req.body.name;
+    var type = req.body.type;
+    var smoking = req.body.smoking;
+    var seats = req.body.seats;
+    var availability = req.body.availability;
+    var leaseterm = req.body.leaseterm;
+    var price = req.body.price;
+    var listed = req.body.listed;
+
+    const workspaceToUpdate = obj.Workspaces.find(
+      workspace => workspace.id === id
+    );
+
+    workspaceToUpdate.name = name;
+    workspaceToUpdate.type = type;
+    workspaceToUpdate.smoking = smoking;
+    workspaceToUpdate.seats = seats;
+    workspaceToUpdate.availability = availability;
+    workspaceToUpdate.leaseterm = leaseterm;
+    workspaceToUpdate.price = price;
+    workspaceToUpdate.listed = listed;
+
+    let data = JSON.stringify(obj, null, 1);
+    fs.writeFile('data/data.json', data, Updated);
+    function Updated() {
+      console.log('Workspace updated.');
+      res.send('Workspace has been updated.');
     }
-  });
+  }
+});
 
 // ROUTE TO "DELETE WORKSPACE" PAGE
 app.get('/workspace/delete', function (req, res) {
-    res.sendFile(__dirname + '/workspace/delete-workspace.html');
-  });
-  
+  res.sendFile(__dirname + '/workspace/delete-workspace.html');
+});
+
 // ROUTE TO "WORKSPACE DELETED" PAGE
 app.post('/workspace/workspace-deleted', urlencodedParser, DeleteWorkspace);
-  
-    // Delete workspace from json logic
-    function DeleteWorkspace(req, res) {
-    // Select workspace dropdown
-    var selectedWorkspace = req.body.types;
-  
-    var json = fs.readFileSync('data/data.json');
-    var array = JSON.parse(json);
-    var Workspace = array.Workspaces;
-    array.Workspaces = Workspace.filter(workspaceObject => {
-      return workspaceObject.name !== selectedWorkspace;
-    });
-    fs.writeFileSync('data/data.json', JSON.stringify(array, null, 2));
-  
-    console.log('Workspace deleted.');
-    res.send(
-      'The workspace called *' + selectedWorkspace + '* was successfully deleted!'
-    );
-  }
+
+// Delete workspace from json logic
+function DeleteWorkspace(req, res) {
+  // Select workspace dropdown
+  var selectedWorkspace = req.body.types;
+
+  var json = fs.readFileSync('data/data.json');
+  var array = JSON.parse(json);
+  var Workspace = array.Workspaces;
+  array.Workspaces = Workspace.filter(workspaceObject => {
+    return workspaceObject.name !== selectedWorkspace;
+  });
+  fs.writeFileSync('data/data.json', JSON.stringify(array, null, 2));
+
+  console.log('Workspace deleted.');
+  res.send(
+    'The workspace called *' + selectedWorkspace + '* was successfully deleted!'
+  );
+}
 
 // API - retrieve all workspaces
 app.get('/workspace/api', function (req, res) {
-    let allWorkspaces = obj.Workspaces;
-    res.send(allWorkspaces);
+  let allWorkspaces = obj.Workspaces;
+  res.send(allWorkspaces);
 });
 
 var server = app.listen(1007, function () {
